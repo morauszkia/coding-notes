a4df34d58e2a419e9fe0085d9effd683fce0fa5d
András Morauszki <morauszki.andras@gmail.com>
2025-02-13 16:05:44 +0100

    Change sidebar links, and add social links

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,28 +1,41 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "My Treasury of Coding Notes",
-  description: "A site to house all my coding notes for various languages, frameworks and libraries that I came into contact",
+  description:
+    "A site to house all my coding notes for various languages, frameworks and libraries that I came into contact",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "JS/TS",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "JavaScript", link: "/js/" },
+          { text: "TypeScript", link: "/ts/" },
+        ],
+      },
+      {
+        text: "Frontend Frameworks",
+        items: [
+          { text: "React/Next.js", link: "/react-next/" },
+          { text: "Vue/Nuxt.js", link: "/vue-nuxt/" },
+          { text: "Svelte.js", link: "/svelte/" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/morauszkia" },
+      {
+        icon: "linkedin",
+        link: "https://www.linkedin.com/in/andras-morauszki/",
+      },
+    ],
+  },
+});
